85c2b7db3ec2d6aea09727fe1f0d19ad98ebb9fb
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-07-29 10:49:04 +0200

    finishing exercise 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-var phraseSplitterEs5 = phrase => phrase.split(" ");
-console.log(phraseSplitterEs5("Hello the weather is nice"));
+// var phraseSplitterEs5 = phrase => phrase.split(" ");
+// console.log(phraseSplitterEs5("Hello the weather is nice"));
 
 
+var docLogEs5 = () => {console.log(document);};
+docLogEs5();
+
+
+
+
+
+// var docLogEs5 = function docLog() {
+//    console.log(document);
+// };
 
 
 
