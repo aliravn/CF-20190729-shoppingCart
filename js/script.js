b788da1a964d0e4378890d2a2f3dca20a5289b45
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-07-29 10:38:58 +0200

    finishing basic exercise 1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var phraseSplitterEs5 = phrase => phrase.split(" ");
+console.log(phraseSplitterEs5("Hello the weather is nice"));
+
+
+
+
+
+
+// var phraseSplitterEs5 = function phraseSplitter(phrase) {
+//  return phrase.split(' ');
+// };
