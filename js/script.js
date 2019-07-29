0527582136c1b858fca051d4f92deb83e844e9ac
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-07-29 10:55:43 +0200

    finishing basic exercise 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// var phraseSplitterEs5 = phrase => phrase.split(" ");
-// console.log(phraseSplitterEs5("Hello the weather is nice"));
+
+var setNameIdsEs5 = (id, name) => ({id: id, name: name});
+console.log(setNameIdsEs5(1, "Alex"));
+
+
+
+
+// var setNameIdsEs5 = function setNameIds(id, name) {
+
+//  return {
+
+//    id: id,
+
+//    name: name
+
+//  };
+
+// };
+
+
+
+
+
+
+
+
+
+var phraseSplitterEs5 = phrase => phrase.split(" ");
+console.log(phraseSplitterEs5("Hello the weather is nice"));
 
 
 var docLogEs5 = () => {console.log(document);};
@@ -9,13 +36,13 @@ docLogEs5();
 
 
 
-// var docLogEs5 = function docLog() {
-//    console.log(document);
-// };
+// // var docLogEs5 = function docLog() {
+// //    console.log(document);
+// // };
 
 
 
 
-// var phraseSplitterEs5 = function phraseSplitter(phrase) {
-//  return phrase.split(' ');
-// };
+// // var phraseSplitterEs5 = function phraseSplitter(phrase) {
+// //  return phrase.split(' ');
+// // };
