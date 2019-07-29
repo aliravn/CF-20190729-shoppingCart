de669f617095f754f69092da6ac77207ae441fe0
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-07-29 11:02:47 +0200

    finishing the basic exercises

--- js/script.js
+++ js/script.js
@@ -1,48 +1,69 @@
 
-var setNameIdsEs5 = (id, name) => ({id: id, name: name});
-console.log(setNameIdsEs5(1, "Alex"));
+
+var sum = (x,y) => {var sum = x + y;
+	var diff = x - y;
+	var result = sum * diff;
+	console.log(result);}
+
+sum(2,3);
+
+
+// function sum(x,y){
+// 	var sum = x + y;
+// 	var diff = x - y;
+// 	var result = sum * diff;
+// 	console.log(result);
+// }
+
+// sum(4, 3);
 
 
 
 
-// var setNameIdsEs5 = function setNameIds(id, name) {
-
-//  return {
-
-//    id: id,
-
-//    name: name
-
-//  };
-
-// };
+// var setNameIdsEs5 = (id, name) => ({id: id, name: name});
+// console.log(setNameIdsEs5(1, "Alex"));
 
 
 
 
+// // var setNameIdsEs5 = function setNameIds(id, name) {
 
+// //  return {
 
+// //    id: id,
 
+// //    name: name
 
+// //  };
 
-var phraseSplitterEs5 = phrase => phrase.split(" ");
-console.log(phraseSplitterEs5("Hello the weather is nice"));
-
-
-var docLogEs5 = () => {console.log(document);};
-docLogEs5();
-
-
-
-
-
-// // var docLogEs5 = function docLog() {
-// //    console.log(document);
 // // };
 
 
 
 
-// // var phraseSplitterEs5 = function phraseSplitter(phrase) {
-// //  return phrase.split(' ');
-// // };
+
+
+
+
+
+// var phraseSplitterEs5 = phrase => phrase.split(" ");
+// console.log(phraseSplitterEs5("Hello the weather is nice"));
+
+
+// var docLogEs5 = () => {console.log(document);};
+// docLogEs5();
+
+
+
+
+
+// // // var docLogEs5 = function docLog() {
+// // //    console.log(document);
+// // // };
+
+
+
+
+// // // var phraseSplitterEs5 = function phraseSplitter(phrase) {
+// // //  return phrase.split(' ');
+// // // };
